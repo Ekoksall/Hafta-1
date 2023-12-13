34e6f43eb5847252bac1b6634ecd250a52062a1d
Enver <enverkksl006@gmail.com>
2023-12-14 01:42:34 +0300

    Update Main.java

--- Manav Kasa Program/src/Main.java
+++ Manav Kasa Program/src/Main.java
@@ -1,16 +1,1 @@
 
-import java.util.Scanner;
-
-public class Main {
-    public static void main(String[] args) {
-            double toplam ,armut = 2.14 , elma = 3.67 , domates = 1.11 , muz = 0.95,  patlıcan = 5.0 ;
-            int kilo;
-
-            Scanner mnv = new Scanner(System.in);
-            System.out.print("Kaç Kilo Armut Aldınız : ");
-            mnv = ind
-
-
-
-    }
-}
