d2f3fd029169a622a7944e5fd4c1aa3081cdb598
Enver <enverkksl006@gmail.com>
2023-12-21 03:14:49 +0300

    Ucak Bileti Fiyati

--- Ucak Bileti Fiyati/src/Main.java
+++ Ucak Bileti Fiyati/src/Main.java
@@ -6,20 +6,33 @@ public class Main {
 
         Scanner uck = new Scanner(System.in);
 
-        System.out.print("Gideceğiniz Mesafeyi KM Cinsinden Yazınız :");
+        System.out.print("Gideceğiniz Mesafeyi KM Cinsinden Yazınız: ");
         mesafe = uck.nextInt();
-        System.out.print("Lütfen Yaşınızı Giriniz :");
+        System.out.print("Lütfen Yaşınızı Giriniz: ");
         yas = uck.nextInt();
-        System.out.println("Lütfen Bilet Türünü Seçiniz :");
+        System.out.println("Lütfen Bilet Türünü Seçiniz:");
         System.out.println("1) Tek Yön\n2) Gidiş Dönüş");
-        System.out.print("Seçiminiz :");
+        System.out.print("Seçiminiz: ");
         secim = uck.nextInt();
+
         if (secim == 1 || secim == 2) {
-            System.out.print("Seçiminiz :" + secim);
+            double fiyat = mesafe * 0.10;
+
+            if (yas < 12) {
+                fiyat *= 0.5;
+            } else if (yas >= 12 && yas <= 24) {
+                fiyat *= 0.9;
+            } else if (yas >= 65) {
+                fiyat *= 0.7;
+            }
+
+            if (secim == 2) {
+                fiyat *= 0.8;
+            }
+
+            System.out.println("Toplam Bilet Fiyatı: " + fiyat + " TL");
         } else {
-            System.out.print("Lütfen Geçerli Bir Sayı Giriniz!");
+            System.out.println("Lütfen Geçerli Bir Sayı Giriniz!");
         }
-
-
     }
 }
